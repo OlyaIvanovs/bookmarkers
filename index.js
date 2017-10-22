127426c0cbe6ce89cf3056c5cb69bcd6b0d14d3f
Olya Ivanova <olya@ivanovs.info>
2017-10-23 07:33:19 +1300

    Second

--- index.js
+++ index.js
@@ -1,4 +1,27 @@
-document.getElementById('myForm').addEventListener('submit', saveBookmark);
+var load = new Promise(
+    function(resolve, reject) {
+        window.onload = resolve;
+    }
+);
+
+var ask = function() {
+    load.then(function(fulfilled) {
+        fetchBookmarks();
+        document.getElementById('myForm').addEventListener('submit', saveBookmark);
+        var deleteBookmarkButns = document.getElementsByClassName('delete-bookmark');
+        for (var k = 0; k < deleteBookmarkButns.length; k++) {
+            deleteBookmarkButns[k].onclick = function() {
+                deleteBookmark.call(this.previousElementSibling);
+            };
+        }
+    })
+    .catch(function() {
+        console.log(error.message);
+    });
+} 
+
+ask();
+
 
 function saveBookmark(evt) {
     var siteName = document.getElementById('siteName').value;
@@ -23,5 +46,18 @@ function saveBookmark(evt) {
 
 function fetchBookmarks() {
     var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
-    
+    var source   = document.getElementById('bookmarker-template').innerHTML;
+    var bookmarksCont = document.getElementById('bookmarkers_list');
+    var templateBookmarks = Handlebars.compile(source);
+    bookmarksCont.innerHTML = templateBookmarks({bookmarks: bookmarks});
+}
+
+function deleteBookmark() {
+    var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+    var urlForDelete = this.href;
+    for (var k = 0; k < bookmarks.length; k++) {
+        if (bookmarks[k].url == urlForDelete) {
+            console.log(bookmarks[k].url);
+        }
+    }
 }
